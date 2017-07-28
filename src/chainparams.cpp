c0f056422d27db50ef925ca1db2954a5409b96ad
kuldeep <30282127+kuldeepsinghsaini108@users.noreply.github.com>
2017-07-28 16:29:46 +0530

    string fixes

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -135,7 +135,7 @@ public:
         genesis.nNonce = 0;
 
         hashGenesisBlock = genesis.GetHash();
-		printf(hashGenesisBlock.c_str());
+		//printf(hashGenesisBlock);
         assert(hashGenesisBlock == uint256("0x0"));
         assert(genesis.hashMerkleRoot == uint256("0x"));
 
